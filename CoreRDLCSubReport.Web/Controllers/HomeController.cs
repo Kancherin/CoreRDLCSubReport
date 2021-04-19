46f1713be368b75de2356430d9e55b477ca13a31
David <david_horeb@hotmail.com>
2021-04-19 07:34:19 -0300

    Add Proyect WindowsForms And Create Report

--- CoreRDLCSubReport.Web/Controllers/HomeController.cs
+++ CoreRDLCSubReport.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using CoreRDLCSubReport.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Reporting.NETCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,15 +25,44 @@ namespace CoreRDLCSubReport.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult PrintReport()
         {
+            string renderFormat = "PDF";
+            string extension = "pdf";
+            string mimetype = "application/pdf";
+            using var report = new LocalReport();
+
+            var dt = new DataTable();
+            dt = GetEmpleados();
+
+            report.DataSources.Add(new ReportDataSource("dsEmpleados", dt));
+            var parameters = new[] { new ReportParameter("param1", "RDLC Sub-Report Ejemplo") };
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+
+        public DataTable GetEmpleados()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Nombre");
+            dt.Columns.Add("Departamento");
+            dt.Columns.Add("Cargo");
+
+            DataRow row;
+            for (int i = 100; i < 150; i++)
+            {
+                row = dt.NewRow();
+                row["Id"] = i;
+                row["Nombre"] = "Empleado " + i;
+                row["Departamento"] = "Sistemas de Información";
+                row["Cargo"] = "Desarrollador";
+                dt.Rows.Add(row);
+            }
+            return dt;
         }
+
+        
     }
 }
