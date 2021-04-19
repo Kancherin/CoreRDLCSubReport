eed0f0b5a86071737795a3844bd9093fb5649a4e
David <david_horeb@hotmail.com>
2021-04-19 10:26:34 -0300

    Reporte Fin

--- CoreRDLCSubReport.Web/Controllers/HomeController.cs
+++ CoreRDLCSubReport.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CoreRDLCSubReport.Web.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Reporting.NETCore;
@@ -14,10 +15,12 @@ namespace CoreRDLCSubReport.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -37,10 +40,26 @@ namespace CoreRDLCSubReport.Web.Controllers
 
             report.DataSources.Add(new ReportDataSource("dsEmpleados", dt));
             var parameters = new[] { new ReportParameter("param1", "RDLC Sub-Report Ejemplo") };
+            report.ReportPath = $"{_webHostEnvironment.WebRootPath}\\Reports\\rptEmpleados.rdlc";
+            report.SetParameters(parameters);
 
-            return View();
+            //for sub report
+            report.SubreportProcessing += new SubreportProcessingEventHandler(SubReportProcessing);
+            //
+
+            var pdf = report.Render(renderFormat);
+            return File(pdf, mimetype, "report." + extension);
         }
 
+        void SubReportProcessing(object sender, SubreportProcessingEventArgs e)
+        {
+            int id = int.Parse(e.Parameters["Id"].Values[0].ToString());
+            var dt2 = new DataTable();
+            dt2 = GetEmpleadosDetalle().Select("Id=" + id).CopyToDataTable();
+
+            ReportDataSource ds = new ReportDataSource("dsEmpleadosDetalles", dt2);
+            e.DataSources.Add(ds);
+        }
 
         public DataTable GetEmpleados()
         {
@@ -63,6 +82,24 @@ namespace CoreRDLCSubReport.Web.Controllers
             return dt;
         }
 
-        
+        public DataTable GetEmpleadosDetalle()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Detalles");
+            DataRow row;
+            for (int i = 100; i < 150; i++)
+            {
+                for (int j = 1; j < 3; j++)
+                {
+                    row = dt.NewRow();
+                    row["Id"] = i;
+                    row["Detalles"] = "Detalle " + j;
+                    dt.Rows.Add(row);
+                }
+            }
+            return dt;
+        }
+
     }
 }
